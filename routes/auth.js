733df3c82ebf2798a3b0f70afd88a87362f60644
shahil-leo <shahilcode@gmail.com>
2022-12-20 14:25:44 +0530

    added the password using crypto js

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,17 @@
 // always  remember logged in using mshahilk28@gmail.com in mongodb
 const router=require('express').Router();
 const User=require('../models/User')
+const CryptoJs=require('crypto-js')
 // REGISTER
 router.post('/register', async (req,res)=>{
     const newUser= new User ({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password,
+        // 
+        password:CryptoJs.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+            ).toString(),
     });
  try{
      const  savedUser = await newUser.save()

36303a642e81eb9f39c1dc984432587a3b16b28e
shahil-leo <shahilcode@gmail.com>
2022-12-21 18:54:37 +0530

    Added the login and register

--- routes/auth.js
+++ routes/auth.js
@@ -1,53 +1,67 @@
 
 // always  remember logged in using mshahilk28@gmail.com in mongodb
-const router=require('express').Router();
-const User=require('../models/User')
-const CryptoJs=require('crypto-js')
+const router = require('express').Router();
+const User = require('../models/User')
+
+const crypto = require('crypto')
+
+const algorithm = 'aes256'; // or any other algorithm supported by OpenSSL
+const key = 'password';
+
+
+
+
+// register user
+
+
+// const CryptoJs=require('crypto-js')
 
 // REGISTER
-router.post('/register', async (req,res)=>{
-    const newUser= new User ({
-        username:req.body.username,
-        email:req.body.email,
-        password:CryptoJs.AES.encrypt(
-            req.body.password,
-            process.env.PASS_SEC
-            ).toString(),
+router.post('/register', async (req, res) => {
+
+    const user = req.body
+    // encrypt password
+    const cipher = crypto.createCipher(algorithm, key);
+    const encrypted = cipher.update(user.password, 'utf8', 'hex') + cipher.final('hex');
+
+    const newUser = new User({
+        username: user.username,
+        email: user.email,
+        password: encrypted,
     });
- try{
-     const  savedUser = await newUser.save()
-     const shahil={username,password}=savedUser
-     res.status(201).json(username)
-     console.log('database is ready')
- }catch(e){
-     res.status(500).json(e)
-     console.log(e)
- }
+
+    try {
+        const savedUser = await newUser.save()
+        const { id, username, password } = savedUser
+        res.status(201).json(id)
+        console.log('user has been created', savedUser)
+    } catch (e) {
+        res.status(500).json(e)
+        console.log(e)
+    }
 
 });
 
-// LOGIN
+// // LOGIN
 
-router.post('/login', async (req,res)=>{
-    try{
-    // we are getting the user from the find one method and we will get the full user from that username
-        const user=await User.findOne({username:req.body.username});
-        !user && res.status(401).json('Wrong credentials')
-// then we will get the database password and everything about the user then we will try to decrypt using the env code
-        const hashedPassword=CryptoJS.AES.decrypt(
-            user.password, 
-            process.env.PASS_SEC);
-        console.log(hashedPassword)
-        // teh password will be in quotes And changed the password from the db  into string 
-        const password=hashedPassword.toString(CryptoJs.enc.Utf8 )
-        console.log(password)
-        password !==req.body.password  && res.status(401).json('wrong credientials')
-    res.status(200).json(user)
-    }catch(err){
-    res.status(500).json(err)
-    } 
+router.post('/login', async (req, res) => {
+    console.log('entering in the login section')
+    const user = req.body
+
+
+    try {
+        const savedUser = await User.findOne({ username: user.username });
+        const decipher = crypto.createDecipher(algorithm, key);
+        const decrypted = decipher.update(savedUser.password, 'hex', 'utf8') + decipher.final('utf8');
+
+
+        if(decrypted === user.password)return res.status(200).json(savedUser)
+        return res.status(404).send('User not found');
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 
-module.exports=router;
+module.exports = router;
 

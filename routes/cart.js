9252f6136101eea6dc734756628cbfab867826e1
shahil-leo <shahilcode@gmail.com>
2022-12-26 01:37:26 +0530

    added the cart section completely

--- routes/cart.js
+++ routes/cart.js
@@ -4,27 +4,31 @@ const {
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin,
 } = require("./verifyToken");
-
 const router = require("express").Router();
+
+// localhost:5000/api/cart this is the routing of the cart 
 
 //CREATE
 
 router.post("/", verifyToken, async (req, res) => {
-  const newCart = new Cart(req.body);
-
+    // we will accept all the req from the post method and then we will store that in the newCart variable
+  const newCart = new Cart(req.body); 
   try {
+    // saving in the db is the next step if we have any problem while saving into the db then it will come to the catch 
     const savedCart = await newCart.save();
     res.status(200).json(savedCart);
   } catch (err) {
     res.status(500).json(err);
+    console.log(err)
   }
 });
 
 //UPDATE
+// id means we want to put the id of which persons cart we want to update
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const updatedCart = await Cart.findByIdAndUpdate(
-      req.params.id,
+    const updatedCart = await Cart.findByIdAndUpdate(req.params.id,
+        // want to ask about this to sir
       {
         $set: req.body,
       },
@@ -47,9 +51,11 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 //GET USER CART
-router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+// verifyToken and authentication enn parayumbo token first verify cheyyanm enit athil nin oru user kittanm aa userum namal param koduknnaa userum crt anho nokkum anenkil mathram nadakolluu
+
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const cart = await Cart.findOne({ userId: req.params.userId });
+    const cart = await Cart.findOne({ userId: req.params.id });
     res.status(200).json(cart);
   } catch (err) {
     res.status(500).json(err);

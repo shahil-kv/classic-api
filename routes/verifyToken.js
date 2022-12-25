007d41e18fe8faf513814974f1d2ce767b40d33d
shahil-leo <shahilcode@gmail.com>
2022-12-26 01:37:56 +0530

    verifiation of admin or user and authentication is succesfull

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -10,18 +10,21 @@ const verifyToken=(req,res,next)=>{
        jwt.verify(token,secretKey,(err,user)=>{
         if(err) res.status(403).json('Token is not valid')
         // we get the user from using the secret key and the token they provide if that is an error then we will catch it and show to the user
-        // console.log(user)
          req.user=user
          next();
        })
-    //    if the users token is not true then this is will happen example if the token is wrong
+// if the users token is not true then this is will happen example if the token is wrong
     }else{
         return res.status(401).json('You are not authenticated or if the token and the id is not provided')
     }
 }
-
+// we wnat to ask about this also
   const verifyTokenAndAuthorization=(req,res,next)=>{
     verifyToken(req,res,()=>{
+ // namuk oru id kittum from the token aa token ullaa usere namuk kittum =req.user.id
+//  namuk parameter aayit thannaa idyum match anho nokkum 
+        console.log(`from token${req.user.id}`)
+        console.log(req.params.id)
         if(req.user.id===req.params.id || req.user.isAdmin){
            next()
         }else{
@@ -33,6 +36,7 @@ const verifyToken=(req,res,next)=>{
   const verifyTokenAndAdmin=(req,res,next)=>{
     verifyToken(req,res,()=>{
         if(req.user.isAdmin){
+            console.log('shahil is a kiladi')
            next()
         }else{
             res.status(403).json("you are not allowed to do that")

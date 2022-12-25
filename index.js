b0a0c96b40efa9c1611519f151ff6b54855cb58c
shahil-leo <shahilcode@gmail.com>
2022-12-25 18:08:30 +0530

    added the product router in the inex.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute=require('./routes/user')
 const authRoute=require('./routes/auth')
-
+const productRoute=require('./routes/product')
 dotenv.config();
 
 mongoose
@@ -19,6 +19,7 @@ mongoose
   // This is the user Router we need to done
   app.use('/api/users',userRoute);
   app.use('/api/auth',authRoute );
+  app.use('/api/products',productRoute)
   app.listen(5000, () => {
   console.log("Backend server is running on port 5000");
  });

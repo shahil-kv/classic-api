ed2ebbf28bfc7a440d8cbff57d8f9ed14a1c8ef9
shahil-leo <shahilcode@gmail.com>
2022-12-25 14:45:13 +0530

    Added the updating of username of the user

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,24 @@
+const User = require('../models/User');
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
 const router=require('express').Router();
 
- router.put("/:id",)
+ router.put("/:id",verifyTokenAndAuthorization, async (req,res)=>{
+      if(req.body.password){
+        const cipher = crypto.createCipher(algorithm, key);
+        const encrypted = cipher.update(req.body.password, 'utf8', 'hex') + cipher.final('hex');
+       req.body.password=encrypted;
+      }
+
+      try{
+        const updatedUser=await User.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        },{new:true}
+        );
+        res.status(200).json(updatedUser)
+      }catch(err){
+        res.status(500).json(err)
+      }
+ })
 
 module.exports=router
 
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,1 +1,28 @@
 const jwt=require('jsonwebtoken');
+
+const verifyToken=(req,res,next)=>{
+    const authHeader=req.headers.token
+    if(authHeader){
+        const token=authHeader.split(" ")[1]
+        console.log(token)
+       jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
+        if(err) res.status(403).json('Token is not valid')
+         req.user=user
+         next();
+       })
+    }else{
+        return res.status(401).json('You are not authenticated or if the token and the id is not provided')
+    }
+}
+
+  const verifyTokenAndAuthorization=(req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.id===req.params.id || req.user.isAdmin){
+           next()
+        }else{
+            res.status(403).json("you are not allowed to do that")
+        }
+    })
+  }
+ 
+module.exports={verifyToken,verifyTokenAndAuthorization}

2fdd6dc1d9b76add154cea6c37e5eff7665beda4
shahil-leo <shahilcode@gmail.com>
2022-12-25 20:47:21 +0530

    added the cart and order

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute=require('./routes/user')
 const authRoute=require('./routes/auth')
 const productRoute=require('./routes/product')
+const cartRoute=require('./routes/cart')
+const orderRoute = require("./routes/order");
 dotenv.config();
 
 mongoose
@@ -20,6 +22,8 @@ mongoose
   app.use('/api/users',userRoute);
   app.use('/api/auth',authRoute );
   app.use('/api/products',productRoute)
+  app.use('/api/cart',cartRoute)
+  app.use('/api/orders',orderRoute)
   app.listen(5000, () => {
   console.log("Backend server is running on port 5000");
  });
